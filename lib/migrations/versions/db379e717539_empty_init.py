dfe3f0765e68faa673129e4a979176d0f957a9ff
Elisa Shapiro <elisashapiro@gmail.com>
2023-10-12 12:17:58 -0400

    code along

---
+++ lib/migrations/versions/db379e717539_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: db379e717539
+Revises: 
+Create Date: 2023-10-12 12:06:01.939625
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'db379e717539'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
